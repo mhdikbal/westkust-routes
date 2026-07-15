4a17108a4f86af72a16dba1a12c8a47d8f2b077b
Muhammad Ikbal <mhdikbal@gmail.com>
2026-07-15 23:25:10 +0700

    fix: ALLOWED_HOSTS revert to * (env var override for prod)

--- frontend/config/settings.py
+++ frontend/config/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 DEBUG = os.getenv("DEBUG", "False") == "True"
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1,salido.my.id").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
 
 INSTALLED_APPS = [
     "django.contrib.staticfiles",
